c0d2997cf253752343bc5b833f2e2f3eabb10175
Andrii Kochmaruk <luckyanry@gmail.com>
2020-07-27 23:22:46 +0200

    Updates

---
+++ precache-manifest.02cdfd3c8530c3311cd356dc65837d67.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0d4f5af89ed0d56e5d61df8fd9d6024",
+    "url": "/goit-react-hw-05-phonebook/index.html"
+  },
+  {
+    "revision": "9b034abac5f198f9a7f2",
+    "url": "/goit-react-hw-05-phonebook/static/css/main.921a6026.chunk.css"
+  },
+  {
+    "revision": "0d83a5273246543d7a94",
+    "url": "/goit-react-hw-05-phonebook/static/js/2.48a8ae62.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-05-phonebook/static/js/2.48a8ae62.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b034abac5f198f9a7f2",
+    "url": "/goit-react-hw-05-phonebook/static/js/main.87fbbf80.chunk.js"
+  },
+  {
+    "revision": "3f441844d3ba7dba1f3a",
+    "url": "/goit-react-hw-05-phonebook/static/js/runtime-main.61bf2916.js"
+  }
+]);
